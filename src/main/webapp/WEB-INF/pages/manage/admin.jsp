4b91aec86c209803b66395e31e394ad6fe2805c0
lmg518 <1344515375@qq.com>
2017-10-31 13:52:46 +0800

    优化house_edit.js跟4.0一样

--- src/main/webapp/WEB-INF/pages/manage/admin.jsp
+++ src/main/webapp/WEB-INF/pages/manage/admin.jsp
@@ -1,15 +1,13 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@taglib uri= "http://java.sun.com/jsp/jstl/core" prefix= "c" %> 
+<%@ page  contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <c:set var="basePath" value="${pageContext.request.contextPath}"></c:set>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
 <META HTTP-EQUIV="Cache-Control" CONTENT="no-cache">
 <META HTTP-EQUIV="Expires" CONTENT="0">
-<title>Amy精品民宿后台管理</title>
-
+<title>Insert title here</title>
 
 <link rel="stylesheet" href="${basePath}/common/bootstrap/css/bootstrap.min.css" />
 <link rel="stylesheet" href="${basePath}/common/bootstrap/css/bootstrap-datepicker.min.css" />
@@ -22,60 +20,46 @@
 <script src="${basePath}/jquery/jquery-3.2.1.min.js"></script>
 <script src="${basePath}/jquery/jquery.validate.min.js"></script>
 <script src="${basePath}/jquery/jquery.form.js"></script>
- 
 <script src="${basePath}/common/bootstrap/js/bootstrap.min.js"></script>
 <script src="${basePath}/common/bootstrap/js/bootstrap-datepicker.min.js"></script>
 <script src="${basePath}/common/bootstrap/js/bootstrap-table.min.js"></script>
-
-<!--  
 <script src="${basePath}/static/plugins/layer/layer.js"></script>
 <script src="${basePath}/static/plugins/treegrid/jquery.treegrid.min.js"></script>
 <script src="${basePath}/static/plugins/treegrid/jquery.treegrid.bootstrap3.js"></script>
 <script src="${basePath}/static/plugins/treegrid/jquery.treegrid.extension.js"></script>
 <script src="${basePath}/static/plugins/treegrid/tree.table.js"></script>
 <script src="${basePath}/static/plugins/ztree/jquery.ztree.all.min.js"></script>
--->
+
 
 </head>
-
-
 <body>
-
-    
-	<%@ include file="menu.jsp" %>
-	<hr>
-	 <!-- 动态插入数据 --> 
-	  <div id="container">
-	  
-	  </div>
-	
-	
-  <!-- modal dialog -->
-  <div class="modal fade" id="modal-dialog" tabindex="-1" role="dialog" aria-labelledby="modalTitleLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-					<h4 class="modal-title" id="modalTitleLabel">New message</h4>
-				</div>
-				<div class="modal-body"></div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default cancel" data-dismiss="modal">取消</button>
-					<button type="button" class="btn btn-primary ok ">保存</button>
-				</div>
-			</div>
-		</div>
-	</div>
-	
-	
-	
-	
-	
-	
- 
+ <%@ include file="menu.jsp" %>
+ <!--动态的设置内容的容器 -->
+ <div id="container">
+       
+ </div> 
+ <!-- Modal(模态框) -->
+ <div class="modal fade" id="modal-dialog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+   <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+        <button type="button" class="btn btn-primary ok" >保存</button>
+      </div>
+    </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- modal -->
 </body>
-
-
 </html>
+
+
+
+
+
--- src/main/webapp/js/manage/house_edit.js
+++ src/main/webapp/js/manage/house_edit.js
@@ -1,49 +1,33 @@
+//$(document).ready(function(){
+//	
+//	//当模态框隐藏时在.ok上绑定的事件执行解绑动作
+//	$("#modal-dialog").on(
+//	   "hidden.bs.modal",function() {$(this).off('click', '.ok').removeData("id")
+//	});
+//	
+//	//保存或更新操作(在这写 会保存二次)
+//	$("#modal-dialog").on('click','.ok',doSaveOrUpdate);
+//	//获得模态框上绑定的id值
+//	var id=$("#modal-dialog").data("id");
+//	console.log(id);
+//	//假如id有值,说明这是修改,然后根据id获得对象,初始化模态框数据
+//	if(id)doGetObjectById(id);
+//})
+
 $(document).ready(function(){
-	
-	
-	
-	
-	
-	
-	//当模态框隐藏时在.ok上绑定的事件执行解绑动作
-	$("#modal-dialog").on(
-	   "hidden.bs.modal",function() {$(this).off('click', '.ok').removeData("id")
-	});
-	
-	//保存或更新操作(在这写 会保存二次)
-	//$("#modal-dialog").on('click','.ok',doSaveOrUpdate);
-	
-	//保存
-	//$("#modal-dialog").on('click','.ok',doSave);
-	
-	//更新
-	//$("#modal-dialog").on('click','.ok',doUpdate);
-	
-	
+	$("#modal-dialog").on('click','.ok',
+			doSaveOrUpdate);
 	//获得模态框上绑定的id值
 	var id=$("#modal-dialog").data("id");
-	console.log(id);
 	//假如id有值,说明这是修改,然后根据id获得对象,初始化模态框数据
-	//if(id)doGetObjectById(id);
-	if(id){
-		doGetObjectById(id);
-	}else{
-		$("#modal-dialog").on('click','.ok',doSave);
-	}
-	
-	
+	if(id)doGetObjectById(id);
+	//当模态框隐藏时在.ok上绑定的事件执行解绑动作
+	$("#modal-dialog").on(
+	   "hidden.bs.modal",function() {
+		$(this).off('click', '.ok')
+		       .removeData("id")
+	});
 })
-
-
-   //保存或更新操作 （写在自动加载区 会保存二次）   
-//$("#modal-dialog").on('click','.ok',doSaveOrUpdate);
-////获得模态框上绑定的id值
-//var id=$("#modal-dialog").data("id");
-//console.log(id);
-////假如id有值,说明这是修改,然后根据id获得对象,初始化模态框数据
-//if(id)doGetObjectById(id);	
-
-
 
 //根据id查找房源对象
 function doGetObjectById(id){
@@ -54,11 +38,6 @@ function doGetObjectById(id){
 		if(result.state==1){
 			//初始化表单数据
 			doFillFormData(result.data);
-			
-			//更新
-			$("#modal-dialog").on('click','.ok',doUpdate);
-			
-			
 		}else{
 			alert(result.message);
 		}
@@ -72,60 +51,14 @@ function doFillFormData(obj){
 	$("#cityId").val(obj.city);     //城市    根据house_edit.jsp中 <input id>
 	$("#districtId").val(obj.district); //区域
 	
-	
-	
 	/*$("#beginDateId").val(obj.beginDate);//2017/08/09 
 	$("#endDateId").val(obj.endDate);*/
 }
 
-//保存
-function doSave(){
-	//1.获得表单数据
-	var params=doGetEditFormData();
-	var url="house/doSaveProject.do";
-	$.post(url,params,function(result){
-		if(result.state==1){
-			//1)隐藏模态框
-			$("#modal-dialog").modal("hide");
-			//当模态隐藏时，.ok上绑定的事件执行解绑动作  （否则会执行二次保存操作）
-			$('#modal-dialog').modal('hide').off('click', '.ok').removeData("id");
-			//2)重新查询列表数据
-			doGetObjects();
-		}else{
-		   alert(result.message);
-		}
-	});
-	
-}
-//更新
-function doUpdate(){
-	//1.获得表单数据
-	var params=doGetEditFormData();
-	var url="house/doUpdataProject.do";
-	$.post(url,params,function(result){
-		if(result.state==1){
-			//1)隐藏模态框
-			$("#modal-dialog").modal("hide");
-			//当模态隐藏时，.ok上绑定的事件执行解绑动作  （否则会执行二次保存操作）
-			$('#modal-dialog').modal('hide').off('click', '.ok').removeData("id");
-			//2)重新查询列表数据
-			doGetObjects();
-		}else{
-		   alert(result.message);
-		}
-	});
-	
-}
-
-
-
-
-
-
 //保存或更新数据
 function doSaveOrUpdate(){
+	debugger;
 	if($("#editFormId").valid()){//required
-		
 	//1.获得表单数据
 	var params=doGetEditFormData();
 	//2.将数据提交到服务端
@@ -135,9 +68,6 @@ function doSaveOrUpdate(){
 		if(result.state==1){
 			//1)隐藏模态框
 			$("#modal-dialog").modal("hide");
-			
-			//当模态隐藏时，.ok上绑定的事件执行解绑动作  （否则会执行二次保存操作）
-			$('#modal-dialog').modal('hide').off('click', '.ok').removeData("id");
 			console.log('lmg')
 			//2)重新查询列表数据
 			doGetObjects();
@@ -145,8 +75,7 @@ function doSaveOrUpdate(){
 		   alert(result.message);
 		}
 	});
-	
-	}
+  }
 }
 //获得表单数据
 function doGetEditFormData(){
@@ -156,7 +85,6 @@ function doGetEditFormData(){
 		"title":$("#titleId").val(),
 		"city":$("#cityId").val(),
 		"district":$("#districtId").val(),
-		
 		
 		/*"beginDate":$("#beginDateId").val(),
 		"endDate":$("#endDateId").val(),
